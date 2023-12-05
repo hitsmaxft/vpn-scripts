8f8e56c10cb0315df84f6f3d4163e4edfc78cc16
hitsmaxft <mfthits@gmail.com>
2023-12-06 01:37:22 +0800

    Create set_mode_filter.js

---
+++ set_mode_filter.js
@@ -0,0 +1,19 @@
+const dict = { "running_mode": "filter" };
+const message = {
+    action: "set_running_mode",
+    content: dict
+};
+
+$configuration.sendMessage(message).then(resolve => {
+    if (resolve.error) {
+        console.log(resolve.error);
+    }
+    if (resolve.ret) {
+        let output=JSON.stringify(resolve.ret);
+        console.log(output);
+    }
+    $done();
+}, reject => {
+    // Normally will never happen.
+    $done();
+});
